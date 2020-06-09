469b1619e6a8d1ccbe340214f42748ecc270dc3c
Majid <mohammadmajid55@gmail.com>
2020-06-09 18:57:17 -0400

    adding html and base js

---
+++ app.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
+const input = document.querySelector('#item');
+
+const liMaker = (text) => {
+    const li = document.createElement('li');
+    li.textContent = text;
+    ul.appendChild(li);
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    liMaker(input.value);
+    input.value = '';
+})
+

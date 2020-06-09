c4ca9e1a4cd71d6d01dd1b8b8ad18f7da14b8e30
Majid <mohammadmajid55@gmail.com>
2020-06-09 19:41:18 -0400

    adding local storage to the app and setting up clear all button

--- app.js
+++ app.js
@@ -7,11 +7,34 @@ const liMaker = (text) => {
     const li = document.createElement('li');
     li.textContent = text;
     ul.appendChild(li);
+};
+
+let itemsArray;
+if (localStorage.getItem('items')) {
+    itemsArray = JSON.parse(localStorage.getItem('items'));
+} else {
+    itemsArray = [];
 }
+localStorage.setItem('items', JSON.stringify(itemsArray))
+
+const data = JSON.parse(localStorage.getItem('items'));
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+    itemsArray.push(input.value);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
     liMaker(input.value);
     input.value = '';
-})
+});
+
+data.forEach(item => {
+    liMaker(item);
+});
+
+button.addEventListener('click', function() {
+    localStorage.clear();
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    };
+});
 
